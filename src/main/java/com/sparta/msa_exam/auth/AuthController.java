58c16a1d5cc56ebcc974026409ba4ef604a563ac
“devwonny” <devwonny@gmail.com>
2024-08-11 15:34:10 +0900

    Rest Api Request Test Done

--- src/main/java/com/sparta/msa_exam/auth/AuthController.java
+++ src/main/java/com/sparta/msa_exam/auth/AuthController.java
@@ -1,11 +1,15 @@
 package com.sparta.msa_exam.auth;
 
+import com.sparta.msa_exam.auth.core.User;
+import com.sparta.msa_exam.auth.dto.SignInRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
 	private final AuthService authService;
@@ -20,12 +24,21 @@ public class AuthController {
 	 * @param userId 사용자 ID
 	 * @return JWT 액세스 토큰을 포함한 AuthResponse 객체를 반환합니다.
 	 */
-	@GetMapping("/auth/signIn")
-	public ResponseEntity<?> createAuthenticationToken(@RequestParam String userId) {
+	@PostMapping("/signIn")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody SignInRequest signInRequest) {
+		final String userId = signInRequest.getUserId();
+
 		if (userId == null || userId.trim().isEmpty()) {
 			return ResponseEntity.badRequest().body("User ID must not be null or empty");
 		}
-		return ResponseEntity.ok(new AuthResponse(authService.createAccessToken(userId)));
+		String token = authService.signIn(signInRequest.getUserId(), signInRequest.getPassword());
+		return ResponseEntity.ok(new AuthResponse(token));
+	}
+
+	@PostMapping("/signUp")
+	public ResponseEntity<?> signUp(@RequestBody User user) {
+		User createdUser = authService.signUp(user);
+		return ResponseEntity.ok(createdUser);
 	}
 
 
